00dbf3feafb7529e4a3b45615c5f28e4e224954e
geumsong <rmathd23@gmail.com>
2023-04-14 02:52:28 +0900

    [파일수정]

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
+import "./styles/app.css";
+
 function App() {
   return (
     <div>
-      <div className="top">
-        <span className="logo" src="" href="" />
-        <span className="categories">
-          <span className="about">ABOUT</span>
-          <span className="menu">MENU</span>
-          <span className="news">NEWS</span>
-          <span className="service">SERVICE</span>
-        </span>
+      <div className="header">
+        <image className="logo" src="" alt="로고" />
+        <div className="categories">
+          <span className="nav">ABOUT</span>
+          <span className="nav">MENU</span>
+          <span className="nav">NEWS</span>
+          <span className="nav">SERVICE</span>
+        </div>
       </div>
 
-      <div className="mainImage" src="">
-        <div className="newMenuButton" />
-        <div button onclick={() => (src = "")}>
-          신메뉴 보러가기
-          <div className="roundBox" />
+      {/* 메인이미지 */}
+      <div>
+        <image className="mainImage" src="" alt="메인이미지" />
+        <div className="mainImageTitle">
+          <span className="smallTitleText">여름시즌 신메뉴 출시!</span>
+          <image className="bigTitleText" src="" alt="제목" />
+          <div> 신 메뉴 보러가기 </div>
         </div>
-        <div className="coffeeOneImage" src="" />
-        <div className="coffeeOneText">
-          <p>에메랄드 라떼</p>
-          <p>Emerald Latte</p>
-          <p>코코넛과 레몬, 그리고 초코를 넣은 \ 상큼 달달한 라떼</p>
-        </div>
-        <div className="coffeeTwoImage" src="" />
-        <div className="coffeeOneText">
-          <p>초코바다 라떼</p>
-          <p>Choco Beach Latte</p>
-          <p>초코시럽과 블루티를 넣어 \ 섞은 에티오피아 원두로 만든 라떼</p>
-        </div>
-        <div className="mainBackground" src="" />
+      </div>
+
+      {/* 줄바꿈하는방법? */}
+      <div className="EmeraldLatteImage" src="" alt="에메랄드라떼" />
+      <div className="EmeraldLat`te">
+        <span className="name"> 에메랄드 라떼</span>
+        <span className="englishName">Emerald Latte</span>
+        <span className="explanation">
+          코코넛과 레몬, 그리고 초코를 넣은 상큼 달달한 라떼
+        </span>
+      </div>
+      <div className="chocoBeachLatteImage" src="" alt="초코비치라떼" />
+      <div className="chocoBeachLatte">
+        <span className="name">초코 비치 라떼</span>
+        <span className="englishName">Choco Beach Latte</span>
+        <span className="explanation">
+          초코시럽과 블루티를 넣어 섞은 에티오피아 원두로 만든 라떼
+        </span>
+      </div>
+      <image className="mainBackground" src="" />
+      {/*
         <div className="blackBox">
           <div className="noticeText">공지사항</div>
           <div className="noticeTitle">ㅇㅇㅇ일부 메뉴 가격 인상 안내</div>
@@ -82,7 +94,7 @@ function App() {
             <div className="BeverageImageFour" src="" />
           </div>
         </div>
-      </div>
+      </div>*/}
     </div>
   );
 }
